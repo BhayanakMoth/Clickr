22dd3a01c9df516c987cfb19ff3015261e1f18ff
Moth <ramaninipun@gmail.com>
2019-11-07 18:43:22 +0530

    Final

---
+++ migrations/versions/509a7d1a130e_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 509a7d1a130e
+Revises: ae346256b650, 454fcff3e19f
+Create Date: 2019-10-11 10:03:23.597589
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '509a7d1a130e'
+down_revision = ('ae346256b650', '454fcff3e19f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
